01871ab98c786711889aa705682e838f767ebc5f
Jeremy <bigtiddygothchicks@gmail.com>
2020-12-14 07:49:42 -0700

    added timestamps and csv to microSD module

--- microSD prototype/gateway/gateway.ino
+++ microSD prototype/gateway/gateway.ino
@@ -18,7 +18,11 @@
 
 #include <SPI.h>
 #include <SD.h>
+#include "RTClib.h"
 
+RTC_PCF8523 rtc;
+
+char daysOfTheWeek[7][12] = {"Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"};
 const int chipSelect = 10;
 
 /**
@@ -81,14 +85,45 @@ void onReciveResponse(byte *data, byte len, byte *srcAddr)
 
     Serial.print(" Received integer = ");
     Serial.print(value);
+    Serial.println();
+    DateTime now = rtc.now();
+    Serial.print(now.year(), DEC);
+    Serial.print('/');
+    Serial.print(now.month(), DEC);
+    Serial.print('/');
+    Serial.print(now.day(), DEC);
+    Serial.print(" (");
+    Serial.print(daysOfTheWeek[now.dayOfTheWeek()]);
+    Serial.print(") ");
+    Serial.print(now.hour(), DEC);
+    Serial.print(':');
+    Serial.print(now.minute(), DEC);
+    Serial.print(':');
+    Serial.print(now.second(), DEC);
+    Serial.println();
   
     // open the file. note that only one file can be open at a time,
     // so you have to close this one before opening another.
-    File dataFile = SD.open("datalog.txt", FILE_WRITE);
+    File dataFile = SD.open("datalog.csv", FILE_WRITE);
   
     // if the file is available, write to it:
     if (dataFile) {
-      dataFile.println(value);
+      dataFile.print(value);
+      dataFile.print(',');
+      dataFile.print(now.year(), DEC);
+      dataFile.print('/');
+      dataFile.print(now.month(), DEC);
+      dataFile.print('/');
+      dataFile.print(now.day(), DEC);
+      dataFile.print(',');
+      dataFile.print(now.hour(), DEC);
+      dataFile.print(':');
+      dataFile.print(now.minute(), DEC);
+      dataFile.print(':');
+      dataFile.print(now.second(), DEC);
+      dataFile.println();
+      
+      dataFile.println();
       dataFile.close();
       // print to the serial port too:
       Serial.print(" Saved integer to SD = ");
@@ -143,13 +178,28 @@ void setup()
     ; // wait for serial port to connect. Needed for native USB port only
   }
 
+  if (! rtc.begin()) {
+    Serial.println("Couldn't find RTC");
+    Serial.flush();
+    abort();
+  }
+
+  if (! rtc.initialized() || rtc.lostPower()) {
+    Serial.println("RTC is NOT initialized, let's set the time!");
+    rtc.adjust(DateTime(F(__DATE__), F(__TIME__)));
+  }
+
+  rtc.start();
+  float drift = 43; // seconds plus or minus over oservation period - set to 0 to cancel previous calibration.
+  float period_sec = (7 * 86400);  // total obsevation period in seconds (86400 = seconds in 1 day:  7 days = (7 * 86400) seconds )
+  float deviation_ppm = (drift / period_sec * 1000000); //  deviation in parts per million (μs)
+  float drift_unit = 4.34; // use with offset mode PCF8523_TwoHours
+  int offset = round(deviation_ppm / drift_unit);
+  Serial.print("Offset is "); Serial.println(offset); // Print to control offset
 
   Serial.print("Initializing SD card...");
-
-  // see if the card is present and can be initialized:
   if (!SD.begin(chipSelect)) {
     Serial.println("Card failed, or not present");
-    // don't do anything more:
     while (1);
   }
   Serial.println("card initialized.");
