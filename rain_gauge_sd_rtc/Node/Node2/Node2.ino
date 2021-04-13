45f0b07d513a9828334d45b81cc1d03bb9836296
Justin Leung <justinpc.leung@mail.utoronto.ca>
2021-04-13 13:54:49 -0700

    cleaned up Node code

--- rain_gauge_sd_rtc/Node/Node2/Node2.ino
+++ rain_gauge_sd_rtc/Node/Node2/Node2.ino
@@ -162,28 +162,6 @@ void setup()
   // Set up the callback funtion
   manager->onReceiveRequest(onReceiveRequest);
 
-//
-//  // setup RTC
-//  if (! adalogger_rtc.begin()) {
-//    Serial.println("Couldn't find RTC");
-//    Serial.flush();
-//    abort();
-//  }
-//
-//  if (! adalogger_rtc.initialized() || adalogger_rtc.lostPower()) {
-//    Serial.println("RTC is NOT initialized, let's set the time!");
-//    adalogger_rtc.adjust(DateTime(F(__DATE__), F(__TIME__)));
-//  }
-//
-//  adalogger_rtc.start();
-//  float drift = 43; // seconds plus or minus over oservation period - set to 0 to cancel previous calibration.
-//  float period_sec = (7 * 86400);  // total obsevation period in seconds (86400 = seconds in 1 day:  7 days = (7 * 86400) seconds )
-//  float deviation_ppm = (drift / period_sec * 1000000); //  deviation in parts per million (μs)
-//  float drift_unit = 4.34; // use with offset mode PCF8523_TwoHours
-//  int offset = round(deviation_ppm / drift_unit);
-//  Serial.print("Offset is "); Serial.println(offset); // Print to control offset
-//
-//  lastRainInterrupt = adalogger_rtc.now();
 
   // Set up rain gauge interrupt 
   pinMode(RAIN_GAUGE, INPUT);
